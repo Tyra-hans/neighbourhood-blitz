cb7d2700dc97f0d087b99e477eb0d7c99c90ded2
Tyra-hans <tyrahans17@gmail.com>
2019-12-01 09:40:47 +0300

    Correctly sets up the django project with its app

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/tyra/Desktop/MS-Python-Pre-work/django/neighbourhood/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
