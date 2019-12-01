12b97bbb6892d71d1e7627af1a87d5ba9b78a206
Tyra-hans <tyrahans17@gmail.com>
2019-12-01 10:18:32 +0300

    Creates a view for regular uses to sign up

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import RegularSignUpForm
+from .models import User
+
+class RegularSignUpView(CreateView):
+    model = User
+    form_class = RegularSignUpForm
+    template_name = 'registration/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'regular'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('regulars')
+
+
+
