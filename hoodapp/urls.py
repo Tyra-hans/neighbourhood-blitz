4055bc91d3ac2a33a783d231c1f415737d8730d7
Tyra-hans <tyrahans17@gmail.com>
2019-12-01 18:38:47 +0300

    Installs pillow to allow us to work with images

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -8,3 +8,5 @@ urlpatterns=[
    url('^$', views.landing,name='landing'),
    url('^home/$', views.home, name='home'),  
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
