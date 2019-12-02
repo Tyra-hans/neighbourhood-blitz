13e4702da2c3208c84b97256411d3fec389bd1c6
Tyra-hans <tyrahans17@gmail.com>
2019-12-02 09:50:30 +0300

    Adds a profile view function that will determine how the profile will be displayed

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,29 +1,17 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login
-from .models import User
+from .models import Post, Profile, Business, Neighbourhood
+from django.contrib.auth.models import User
 
-def intern_profile_view(request):
-	
-    if request.method == 'POST':
-		
-        user_form = UserForm(request.POST, prefix='UF')
-        profile_form = RegularProfileForm(request.POST, prefix='PF')
-		
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save(commit=False)
-            user.save()
-            user.regular_profile.bio = profile_form.cleaned_data.get('bio')
-            user.regular_profile.location = profile_form.cleaned_data.get('location')
-            user.regular_profile.save()
-			
-        else:
-            user_form = UserForm(prefix='UF')
-            profile_form = RegularProfileForm(prefix='PF')
-		
-    return render(request, 'regular/reg_profile.html', {'user_form': user_form,'profile_form': profile_form})
 
 def landing(request):
     return render(request,'all-posts/landing.html')
 
 def home(request):
     return render(request, 'all-posts/home.html')
+
+def profile(request, username):
+    user = User.objects.get(username = username)
+    profile = Profile.objects.get(user = user)
+    posts = Post.objects.filter(user = user)
+    return render(request, 'all-posts/profile.html', {'profile': profile, 'posts': posts})
